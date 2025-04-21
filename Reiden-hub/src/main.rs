71b339d545130fdd2fb8dbf7db56adec23fe68b2
Lari4ka <wgrigw@Gmail.com>
2025-04-21 19:42:21 +0300

    civit ai api

--- Reiden-hub/src/main.rs
+++ Reiden-hub/src/main.rs
@@ -1,19 +1,19 @@
 pub mod app;
 pub mod model;
 
-use std::collections::HashMap;
 use dioxus::html::completions::CompleteWithBraces::{button, div, h1, input};
 use dioxus::html::form::action;
 use dioxus::launch;
 use dioxus::logger::tracing;
 use dioxus::logger::tracing::{event, info};
 use dioxus::prelude::*;
-use serde::{Deserialize, Serialize};
+use regex::Regex;
 use serde::de::value::StringDeserializer;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use time::macros;
 use time::macros::date;
 use time::Date;
-use regex::Regex;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const CSS: Asset = asset!("/assets/main.css");
@@ -34,7 +34,7 @@ fn App() -> Element {
             .await
             .unwrap()
     });
-    let url =  match &*img_src.read_unchecked() {
+    let url = match &*img_src.read_unchecked() {
         Some(url) => url.clone(),
         None => String::new(),
     };
