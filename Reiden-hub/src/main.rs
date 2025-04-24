032e7219b46427b1f57ad818fe46810a33eb248f
Lari4ka <wgrigw@Gmail.com>
2025-04-24 13:17:30 +0300

    vse naxui peredelal *peredelau

--- Reiden-hub/src/main.rs
+++ Reiden-hub/src/main.rs
@@ -1,5 +1,4 @@
 pub mod app;
-pub mod model;
 
 use dioxus::html::completions::CompleteWithBraces::{button, div, h1, input};
 use dioxus::html::form::action;
@@ -11,14 +10,12 @@ use dioxus::prelude::*;
 use regex::Regex;
 
 use serde::de::value::StringDeserializer;
-use serde::{Deserialize, Serialize};
 
 use std::collections::HashMap;
 
 use time::macros;
 use time::macros::date;
 use time::Date;
-use crate::model::model::{Span, Spans};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const CSS: Asset = asset!("/assets/main.css");
@@ -30,20 +27,27 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-
-    let mut spans = Spans::new();
-
-    let example_duration = ("01-01-0001".to_string(), "02-02-0002".to_string());
-    let example = Span::new(example_duration);
-    let test = spans.add(example);
-
     rsx! {
-        document::Stylesheet { href: CSS }
-        div {
-            id: "test",
-            for i in spans {
-                "{i}"
-            }
-        }
+
     }
 }
+
+#[component]
+fn Main() -> Element {
+    rsx! {
+        AddComponent {}
+        SpansComponent {}
+    }
+}
+
+#[component]
+fn AddComponent() -> Element {
+    rsx! {}
+}
+
+#[component]
+fn SpansComponent() -> Element {
+    rsx! {}
+}
+
+
